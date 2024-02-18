2fd68b2164eeb079ecfb28522610027683c19a25
https://github.com/030439 <praimcahnd763@gmail.com>
2024-02-18 19:09:06 +0500

    footer includes set

--- html/tunnel/list.php
+++ html/tunnel/list.php
@@ -1,5 +1,5 @@
           
-<?php include('includes/header.php');?>  
+<?php include('../includes/header.php');?>  
 
 <body>
 
@@ -1824,4 +1824,4 @@
 
           
           
-<?php include('includes/footer.php');?>  
+          <?php include('../includes/footer.php');?> 
